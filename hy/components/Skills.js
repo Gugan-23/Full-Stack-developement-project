57c411f34c606e64c2a2eab197eb3cf3f673ac9f
Sanjay R <122106271+SanjayR14@users.noreply.github.com>
2024-07-29 14:52:01 +0530

    Add files via upload

---
+++ hy/components/Skills.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <div className="container">
+        <h2>Skills</h2>
+        <ul>
+          <li>Python</li>
+          <li>Programming in C</li>
+          <li>Java</li>
+          <li>HTML, CSS, and JavaScript</li>
+          <li>DBMS</li>
+          <li>Google Analytics</li>
+          <li>Data Structures in C</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
